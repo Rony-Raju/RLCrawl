182e85478967b7c3732d7d8945d3f48ef43548b1
tjack128 <49701967+tjack128@users.noreply.github.com>
2020-06-09 13:00:54 -0400

    Add files via upload

--- Traccer_Client_Main.py
+++ Traccer_Client_Main.py
@@ -1,42 +1,82 @@
 # This sample code uses the Appium python client
 # pip install Appium-Python-Client
 # Then you can paste this into a file and simply run with Python
+from sqlalchemy import null
 
+import database
+from database import *
 from appium import webdriver
 from random import uniform
 from abstraction import *
 from constants import GUIActionType, SystemActionType, KeyCode, TargetType, TargetState, SelectorType
 from ui_analysis import *
+import sqlite3
+from execution import *
+from uuid import uuid4
+import sqlite3
 
-
+sqliteConnection = sqlite3.connect('Traccar_Client_Database.db')
+cursor = sqliteConnection.cursor()
 done = False
 while not done:
     # Method that starts the application
-    caps = {"platformName": "Android", "deviceName": "Android Emulator", "appPackage": "org.traccar.client",
-            "appActivity": "org.traccar.client.Launcher", "platformVersion": "5.1.1", "noReset": True}
+    caps = {}
+    caps["platformName"] = "Android"
+    caps["deviceName"] = "Android Emulator"
+    caps["appPackage"] = "org.traccar.client"
+    caps["appActivity"] = "org.traccar.client.Launcher"
+    caps["platformVersion"] = "5.1.1"
+    caps["noReset"] = True
 
     driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
 
-    #Getting the current state
-    #print(get_current_state(driver))
+    count = 0
+    sqliteConnection = sqlite3.connect('Traccar_Client_Database.db')
+    cursor = sqliteConnection.cursor()
+    # Getting the current state
+    # print(get_current_state(driver))
 
+    StateID = []
     while True:
-        #Generates the random integer that will be compared to the Home_Button_Probability
+        # Generates the random integer that will be compared to the Home_Button_Probability
         Random = uniform(0.0, 1.0)
 
-        #Home button probability
+        # Home button probability
         Home_Button_Probability = .05
         if Random <= Home_Button_Probability:
-          #At the specified state, generate the home event
-          # " print(driver.query_app_state('org.traccar.client')) " gave us the value of 4 for the current state
-          Selected_event = create_home_event('bb1e312877a55bb7ae166f4e0ba6d0bd84e360d6')
+            # At the specified state, generate the home event
+            # " print(driver.query_app_state('org.traccar.client')) " gave us the value of 4 for the current state
+            Selected_event = create_home_event('bb1e312877a55bb7ae166f4e0ba6d0bd84e360d6')
         else:
-          Current_Events = get_available_events(driver)
-          for events in Current_Events:
+            Current_State = get_current_state(driver)
+            Current_Events = get_available_events(driver)
+            Back_Event = create_back_event('bb1e312877a55bb7ae166f4e0ba6d0bd84e360d6')
+            D = Executor(driver, 10, " ")
+            print("Successfully retrieved the current events")
+            for events in Current_Events:
+                insert_into(count, str(events['actions']), str(events['precondition']['stateId']), 0, 500, 5)
+                count += 1
 
+        Current_Events = get_available_events(driver)
+        Back_Event = create_back_event('bb1e312877a55bb7ae166f4e0ba6d0bd84e360d6')
+        D = Executor(driver, 3, " ")
+        cursor.execute("SELECT RowNumber FROM Traccar_Client_Table WHERE (SELECT MAX(QValues) FROM Traccar_Client_Table)")
+        tuple1 = cursor.fetchone()
+        RowNum = tuple1[0]
+        index = getMaxValueEvent(Current_State)
+        Selected_event = Current_Events[index]
+        D.execute(Selected_event)
 
-
-
-
-
-
+        # # name = " '{'activityName': '.Launcher', 'stateId': 'crash'}' "
+        # # SQL_Testing = " UPDATE Traccar_Client_Table SET EventKey = ? WHERE RowNumber = 0 "
+        # # cursor.execute(SQL_Testing, [name])
+        #
+        # Selected_event['precondition']['stateId'] = 'crash'
+        newState = get_current_state(driver)
+        if newState['stateId'] == 'crash':
+            cursor.execute("UPDATE Traccar_Client_Table SET reward = 0 WHERE (RowNumber = "+index+")")
+            sqliteConnection.commit()
+            cursor.execute("UPDATE Traccar_Client_Table SET QValues = 0 WHERE (EventKey = "+index+")")
+            sqliteConnection.commit()
+        break
+    done = True
---
+++ database.py
@@ -0,0 +1,81 @@
+import sqlite3
+from random import random
+
+try:
+    sqliteConnection = sqlite3.connect('Traccar_Client_Database.db')
+    sqlite_create_table_query = '''CREATE TABLE Traccar_Client_Table (
+                                RowNumber INTEGER PRIMARY KEY,
+                                EventValue TEXT NOT NULL,
+                                EventKey TEXT NOT NULL,
+                                TimesExecuted INTEGER NOT NULL,
+                                QValues REAL NOT NULL, 
+                                Reward REAL
+                                );'''
+
+    cursor = sqliteConnection.cursor()
+    print("Successfully Connected to SQLite")
+    cursor.execute(sqlite_create_table_query)
+    sqliteConnection.commit()
+    print("SQLite table created")
+
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Error while creating a sqlite table", error)
+finally:
+    if (sqliteConnection):
+        sqliteConnection.close()
+        print("sqlite connection is closed")
+
+
+
+
+def insert_into(RowID, EventValue, EventKey, TimesExecutes, QValues, Reward):
+
+
+    try:
+            sqliteConnection = sqlite3.connect('Traccar_Client_Database.db')
+            cursor = sqliteConnection.cursor()
+            print("Connected to SQLite")
+
+            sqlite_insert_with_param = """INSERT INTO Traccar_Client_Table
+                              (RowNumber, EventValue, EventKey, TimesExecuted, QValues, Reward) 
+                              VALUES (?, ?, ?, ?, ?, ?);"""
+
+            data_tuple = (RowID, EventValue, EventKey, TimesExecutes, QValues, Reward)
+            cursor.execute(sqlite_insert_with_param, data_tuple)
+            sqliteConnection.commit()
+            print("Python Variables inserted successfully into SqliteDb_developers table")
+
+            cursor.close()
+
+    except sqlite3.Error as error:
+        print("Failed to insert Python variable into sqlite table", error)
+    finally:
+        if (sqliteConnection):
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+def getMaxValueEvent(stateId):
+    stateId = str(stateId)
+    try:
+            sqliteConnection = sqlite3.connect('Traccar_Client_Database.db')
+            cursor = sqliteConnection.cursor()
+
+            cursor.execute("SELECT rowNumber FROM Traccer_Client_Table WHERE stateId = %s AND"
+                           "(Qvalue = (MAX(Qvalue)))", (stateId))
+
+            numbers = cursor.fetchall()
+            index = random.choice(numbers)
+            cursor.close()
+            return index
+
+    except sqlite3.Error as error:
+        print("Failed to access data from sqlite table", error)
+    finally:
+        if (sqliteConnection):
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+
+
